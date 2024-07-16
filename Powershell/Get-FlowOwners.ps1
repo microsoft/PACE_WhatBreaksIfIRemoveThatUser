c99a6122d568a03946da01a658d3233836d782f5
lavanden <84918071+lavanden@users.noreply.github.com>
2024-07-16 09:43:11 +0200

    Update Get-FlowOwners.ps1

--- Powershell/Get-FlowOwners.ps1
+++ Powershell/Get-FlowOwners.ps1
@@ -1,20 +1,58 @@
 #https://learn.microsoft.com/en-us/power-platform/admin/powerapps-powershell
 #https://learn.microsoft.com/en-us/powershell/module/microsoft.powerapps.administration.powershell/get-adminpowerappconnection?view=pa-ps-latest
 
-$owner=$args[0]
-#$owner="manchesa@eursc.eu"
-#$owner="laurenva@partner.eursc.eu"
+$owner = $args[0]
 
 if (-not (Get-Module Microsoft.PowerApps.Administration.PowerShell -ListAvailable)) {
-    Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -Scope CurrentUser
+    Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -AllowClobber -Scope CurrentUser -Force
+}
+else {
+    #check the version of the installed module and compare it to the latest version
+    $installedVersion = (Get-Module -Name Microsoft.PowerApps.Administration.PowerShell -ListAvailable).Version
+    $latestVersion = New-Object System.Version((Find-Module -Name Microsoft.PowerApps.Administration.PowerShell).Version)
+
+    if ($installedVersion -lt $latestVersion) {
+        Uninstall-Module -Name Microsoft.PowerApps.Administration.PowerShell -Force
+        Install-Module -Name Microsoft.PowerApps.Administration.PowerShell -AllowClobber -Scope CurrentUser -Force 
+    }
+    else {
+        Write-Host "Microsoft.PowerApps.Administration.PowerShell is up to date."
+    }
 }
 
 if (-not (Get-Module Microsoft.PowerApps.PowerShell -ListAvailable)) {
-    Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Scope CurrentUser
+    Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Scope CurrentUser -Force
+}
+else {
+    #check the version of the installed module and compare it to the latest version
+    $installedVersion = (Get-Module -Name Microsoft.PowerApps.PowerShell -ListAvailable).Version
+    $latestVersion = New-Object System.Version((Find-Module -Name Microsoft.PowerApps.PowerShell).Version)
+
+    if ($installedVersion -lt $latestVersion) {
+        Uninstall-Module -Name Microsoft.PowerApps.PowerShell -Force
+        Install-Module -Name Microsoft.PowerApps.PowerShell -AllowClobber -Scope CurrentUser -Force 
+    }
+    else {
+        Write-Host "Microsoft.PowerApps.PowerShell is up to date."
+    }
 }
 
 if (-not (Get-Module Join-Object -ListAvailable)) {
     Install-Module -Name Join-Object
+}
+else {
+    #check the version of the installed module and compare it to the latest version
+    $installedVersion = (Get-Module -Name Join-Object -ListAvailable).Version
+    $latestVersion = New-Object System.Version((Find-Module -Name Join-Object).Version)
+
+    if ($installedVersion -lt $latestVersion) {
+        Uninstall-Module -Name Join-Object -Force
+        Install-Module -Name Join-Object -AllowClobber -Scope CurrentUser -Force
+        #Write-Host "A newer version of the module is available."
+    }
+    else {
+        Write-Host "Join-Object is up to date."
+    }
 }
 
 $ownerId = Get-UsersOrGroupsFromGraph -SearchString $owner | Select-Object -Property ObjectId
@@ -22,27 +60,27 @@ $ownerId = Get-UsersOrGroupsFromGraph -SearchString $owner | Select-Object -Prop
 #ParameterSet is not correctly choosen
 #Blanks check is present in source, passing in blanks will make the function ignore the values 
 $empty = " "
-$flowOwnerRole = Get-AdminFlowOwnerRole -Owner $ownerId.ObjectId -Environment $empty  -FlowName $empty  | Where-Object { $_.RoleType -eq 'Owner' }
+$flowOwnerRole = Get-AdminFlowOwnerRole -Owner $ownerId.ObjectId -Environment $empty -FlowName $empty | Where-Object { $_.RoleType -eq 'Owner' }
+
+$environments = @(Get-AdminPowerAppEnvironment | Select-Object  EnvironmentName, DisplayName)
 
 if ($flowOwnerRole.Count -eq 0) {
     Write-Host "No Flow Owner Role found for $owner"
     exit
 }
 
-$environments = @(Get-AdminPowerAppEnvironment | Select-Object  EnvironmentName, DisplayName)
-
 $allFlows = @(Get-AdminFlow | ForEach-Object {
-    $flow = $_
-    $userId = $flow.CreatedBy.userId
-    [PSCustomObject]@{
-        DisplayName = $flow.DisplayName
-        CreatedTime = $flow.CreatedTime
-        #Enabled = $flow.Enabled
-        #EnvironmentName = $flow.EnvironmentName
-        UserId = [string]$userId
-        FlowId = [string]$flow.FlowName
-    }
-}| Sort-Object -Property FlowId)
+        $flow = $_
+        $userId = $flow.CreatedBy.userId
+        [PSCustomObject]@{
+            DisplayName = $flow.DisplayName
+            CreatedTime = $flow.CreatedTime
+            #Enabled = $flow.Enabled
+            #EnvironmentName = $flow.EnvironmentName
+            UserId      = [string]$userId
+            FlowId      = [string]$flow.FlowName
+        }
+    } | Sort-Object -Property FlowId)
 
 #Need to merge the flow information with the environment information
 $allFlowsWithEnvironment = Join-Object -Left $flowOwnerRole -Right $environments -LeftJoinProperty "EnvironmentName" -RightJoinProperty "EnvironmentName" -ExcludeRightProperties 'EnvironmentName' -Prefix 'Environment_'
@@ -58,4 +96,3 @@ Join-Object -Left $allFlowsWithEnvironment -Right $allFlows -LeftJoinProperty "F
 #Write-Host "Merging Connection information with Environment information"
 #$joined = Join-Object -Left $connections -Right $environments -LeftJoinProperty "EnvironmentName" -RightJoinProperty "EnvironmentName" -ExcludeRightProperties 'EnvironmentName' -Prefix 'Environment_'
 #$joined | Format-Table
-
