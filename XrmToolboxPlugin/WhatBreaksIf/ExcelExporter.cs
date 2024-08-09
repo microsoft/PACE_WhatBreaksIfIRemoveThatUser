060a0538b78a4e15d381a37fd6fe1a6fbd9880b2
Laurens Vandendriessche <lavanden@microsoft.com>
2024-08-09 10:18:06 +0200

    Switch to closedxml for excel generation

--- XrmToolboxPlugin/WhatBreaksIf/ExcelExporter.cs
+++ XrmToolboxPlugin/WhatBreaksIf/ExcelExporter.cs
@@ -1,7 +1,5 @@
-﻿using OfficeOpenXml;
-using OfficeOpenXml.Table;
+﻿using ClosedXML.Excel;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using static WhatBreaksIf.WhatBreaksIfControl;
@@ -27,89 +25,71 @@ namespace WhatBreaksIf﻿
             // get all environments that have either flow or connection references﻿
             var environmentsWithFlows = targetEnvironments.Where(env => env.Key.flows.Any() || env.Key.connectionReferences.Any()).Select(x => x.Key).ToList();
 
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-
             using (var fs = new FileStream(filename, FileMode.Create, FileAccess.Write))
             {
-                using (var package = new ExcelPackage(fs))
+                using (var workbook = new XLWorkbook())
                 {
                     foreach (var environment in environmentsWithFlows)
                     {
                         string flowManagementEnvironment = "https://make.powerautomate.com/";
 
-                        /*switch (environment.location)
-                        {
-                            case "europe":
-                                flowManagementEnvironment = "https://emea.flow.microsoft.com/manage/";
-                                break;
-                            default:
-                                flowManagementEnvironment = "https://flow.microsoft.com/manage/";
-                                break;
-                        }*/
-                        var sheet = package.Workbook.Worksheets.Add(environment.properties.displayName);
+                        var sheet = workbook.AddWorksheet(environment.properties.displayName);
 
-                        // headers
-                        sheet.Cells[1,1].Value = "Type";
-                        sheet.Cells[1, 2].Value = "Id";
-                        sheet.Cells[1, 3].Value = "Name";
-                        sheet.Cells[1, 4].Value = "DisplayName";
-                        sheet.Cells[1, 5].Value = "API";
-                        sheet.Cells[1, 6].Value = "Status";
-                        sheet.Cells[1, 7].Value = "CreatedOn";
-                        sheet.Cells[1, 8].Value = "CreatedBy";
-                        sheet.Cells[1, 9].Value = "ModifiedOn";
-                        sheet.Cells[1, 10].Value = "Url";
+                        sheet.Cell(1, 1).SetValue("Type");
+                        sheet.Cell(1, 2).SetValue("Id");
+                        sheet.Cell(1, 3).SetValue("Name");
+                        sheet.Cell(1, 4).SetValue("DisplayName");
+                        sheet.Cell(1, 5).SetValue("API");
+                        sheet.Cell(1, 6).SetValue("Status");
+                        sheet.Cell(1, 7).SetValue("CreatedOn");
+                        sheet.Cell(1, 8).SetValue("CreatedBy");
+                        sheet.Cell(1, 9).SetValue("ModifiedOn");
+                        sheet.Cell(1, 10).SetValue("Url");
 
                         // Fill the table range with Flow data
                         for (int rowIndex = 2; rowIndex < environment.flows.Count + 1; rowIndex++)
                         {
                             var currentFlow = environment.flows[rowIndex - 1];
-                            sheet.Cells[rowIndex, 1].Value = "Flow";
-                            sheet.Cells[rowIndex, 2].Value = currentFlow.id;
-                            sheet.Cells[rowIndex, 3].Value = currentFlow.name;
-                            sheet.Cells[rowIndex, 4].Value = currentFlow.properties.displayName;
-                            sheet.Cells[rowIndex, 5].Value = currentFlow.properties.apiId.Replace("/providers/Microsoft.PowerApps/apis/", "");
-                            sheet.Cells[rowIndex, 6].Value = currentFlow.properties.state;
-                            sheet.Cells[rowIndex, 7].Value = $"{currentFlow.properties.createdTime.ToShortDateString()} {currentFlow.properties.createdTime.ToLongTimeString()}";
-                            sheet.Cells[rowIndex, 8].Value = currentFlow.properties.creator.objectId;
-                            sheet.Cells[rowIndex, 9].Value = $"{currentFlow.properties.lastModifiedTime.ToShortDateString()} {currentFlow.properties.lastModifiedTime.ToLongTimeString()}";
-                            sheet.Cells[rowIndex, 10].Value = currentFlow.id.Replace("/providers/Microsoft.ProcessSimple/", flowManagementEnvironment);
+                            sheet.Cell(rowIndex, 1).SetValue("Flow");
+                            sheet.Cell(rowIndex, 2).SetValue(currentFlow.id);
+                            sheet.Cell(rowIndex, 3).SetValue(currentFlow.name);
+                            sheet.Cell(rowIndex, 4).SetValue(currentFlow.properties.displayName);
+                            sheet.Cell(rowIndex, 5).SetValue(currentFlow.properties.apiId.Replace("/providers/Microsoft.PowerApps/apis/", ""));
+                            sheet.Cell(rowIndex, 6).SetValue(currentFlow.properties.state);
+                            sheet.Cell(rowIndex, 7).SetValue($"{currentFlow.properties.createdTime.ToShortDateString()} {currentFlow.properties.createdTime.ToLongTimeString()}");
+                            sheet.Cell(rowIndex, 8).SetValue(currentFlow.properties.creator.objectId);
+                            sheet.Cell(rowIndex, 9).SetValue($"{currentFlow.properties.lastModifiedTime.ToShortDateString()} {currentFlow.properties.lastModifiedTime.ToLongTimeString()}");
+                            sheet.Cell(rowIndex, 10).SetValue(currentFlow.id.Replace("/providers/Microsoft.ProcessSimple/", flowManagementEnvironment));
                         }
 
                         // Fill the table range with Connection References data
                         for (int rowIndex = 1; rowIndex <= environment.connectionReferences.Count; rowIndex++)
                         {
                             var currentConnectionReference = environment.connectionReferences[rowIndex - 1];
-                            sheet.Cells[rowIndex + environment.flows.Count, 1].Value = "Connection Reference";
-                            sheet.Cells[rowIndex + environment.flows.Count, 2].Value = currentConnectionReference.id;
-                            sheet.Cells[rowIndex + environment.flows.Count, 3].Value = currentConnectionReference.name;
-                            sheet.Cells[rowIndex + environment.flows.Count, 4].Value = currentConnectionReference.properties.displayName;
-                            sheet.Cells[rowIndex + environment.flows.Count, 5].Value = currentConnectionReference.properties.apiId.Substring(currentConnectionReference.properties.apiId.LastIndexOf('/') + 1);
-                            sheet.Cells[rowIndex + environment.flows.Count, 6].Value = string.Join(",", currentConnectionReference.properties.statuses.Select(s => s.status));
-                            sheet.Cells[rowIndex + environment.flows.Count, 7].Value = $"{currentConnectionReference.properties.createdTime.ToShortDateString()} {currentConnectionReference.properties.createdTime.ToLongTimeString()}";
-                            sheet.Cells[rowIndex + environment.flows.Count, 8].Value = currentConnectionReference.properties.createdBy.email;
-                            sheet.Cells[rowIndex + environment.flows.Count, 9].Value = $"{currentConnectionReference.properties.lastModifiedTime.ToShortDateString()} {currentConnectionReference.properties.lastModifiedTime.ToLongTimeString()}";
-                            sheet.Cells[rowIndex + environment.flows.Count, 10].Value = $"https://make.powerapps.com/environments/{environment.name}/connections/{currentConnectionReference.properties.apiId.Substring(currentConnectionReference.properties.apiId.LastIndexOf('/') + 1)}/{currentConnectionReference.name}/ details";
+                            sheet.Cell(rowIndex + environment.flows.Count, 1).SetValue("Connection Reference");
+                            sheet.Cell(rowIndex + environment.flows.Count, 2).SetValue(currentConnectionReference.id);
+                            sheet.Cell(rowIndex + environment.flows.Count, 3).SetValue(currentConnectionReference.name);
+                            sheet.Cell(rowIndex + environment.flows.Count, 4).SetValue(currentConnectionReference.properties.displayName);
+                            sheet.Cell(rowIndex + environment.flows.Count, 5).SetValue(currentConnectionReference.properties.apiId.Substring(currentConnectionReference.properties.apiId.LastIndexOf('/') + 1));
+                            sheet.Cell(rowIndex + environment.flows.Count, 6).SetValue(string.Join(",", currentConnectionReference.properties.statuses.Select(s => s.status)));
+                            sheet.Cell(rowIndex + environment.flows.Count, 7).SetValue($"{currentConnectionReference.properties.createdTime.ToShortDateString()} {currentConnectionReference.properties.createdTime.ToLongTimeString()}");
+                            sheet.Cell(rowIndex + environment.flows.Count, 8).SetValue(currentConnectionReference.properties.createdBy.email);
+                            sheet.Cell(rowIndex + environment.flows.Count, 9).SetValue($"{currentConnectionReference.properties.lastModifiedTime.ToShortDateString()} {currentConnectionReference.properties.lastModifiedTime.ToLongTimeString()}");
+                            sheet.Cell(rowIndex + environment.flows.Count, 10).SetValue($"https://make.powerapps.com/environments/{environment.name}/connections/{currentConnectionReference.properties.apiId.Substring(currentConnectionReference.properties.apiId.LastIndexOf('/') + 1)}/{currentConnectionReference.name}/details");
                         }
 
-                        //sheet.Cells[1, 7, environment.flows.Count + environment.connectionReferences.Count, 7].Style.Numberformat.Format = "dd/mm/yyyy hh:mm:ss";
-                        //sheet.Cells[1, 7, environment.flows.Count + environment.connectionReferences.Count, 4].Style.Numberformat.Format = "dd/mm/yyyy hh:mm:ss";
-
-                        // Table range including header row
-                        var range = sheet.Cells[1, 1, environment.flows.Count + environment.connectionReferences.Count, 10];
-
                         // create the table
-                        var table = sheet.Tables.Add(range, "DataTable");
+                        var range = sheet.Range(1, 1, environment.flows.Count + environment.connectionReferences.Count, 10);
+                        var table = range.CreateTable("DataTable");
 
-                        // configure the table
-                        table.ShowHeader = true;
-                        table.ShowFirstColumn = true;
-                        table.TableStyle = TableStyles.Medium2;
-                        range.AutoFitColumns();
-
+                        table.Theme = XLTableTheme.TableStyleMedium2;
+                        table.SetShowHeaderRow(true);
+                        table.SetShowRowStripes(true);
+                        table.SetShowAutoFilter(true);
+                        sheet.Columns().AdjustToContents();
                     }
 
-                    package.Save();
+                    workbook.SaveAs(fs);
                 }
             }
         }
