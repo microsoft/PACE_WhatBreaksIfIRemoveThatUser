881ce2d2e671030e5c330ec1db33f6c6f75a9443
Felix Mora <femora@microsoft.com>
2024-08-09 10:05:09 +0200

    prepare for dynamic resolve of missing dependencies

--- XrmToolboxPlugin/WhatBreaksIf/WhatBreaksIf.cs
+++ XrmToolboxPlugin/WhatBreaksIf/WhatBreaksIf.cs
@@ -36,7 +36,7 @@ namespace WhatBreaksIf
         {
             // If you have external assemblies that you need to load, uncomment the following to 
             // hook into the event that will fire when an Assembly fails to resolve
-            // AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolveEventHandler);
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(AssemblyResolveEventHandler);
         }
 
         /// <summary>
